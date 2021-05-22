5760ffd433eee4e5c7cc77c3c55b1be2de42d8be
drain1ma <drain1ma@cmich.edu>
2021-05-22 01:02:23 -0400

    fixed database connection

--- index.php
+++ index.php
@@ -1,12 +1,17 @@
 <!DOCTYPE html> 
 <html> 
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <?php include_once 'connection.inc.php' ?>
 <head> 
     
 </head> 
 <body> 
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
     <nav>
-        <div class="container"> 
+        <div class="container" style="text-align: center"> 
         </div> 
             <a href="games.php">Games</a> 
             <ul>
@@ -26,13 +31,9 @@
         $result = mysqli_query($conn, $sql); 
         $resultCheck = mysqli_num_rows($result); 
 
-
-        if ($resultCheck > 0){
-            while($row = mysqli_fetch_assoc($result)){
-                echo $row['user_name'] . "<br> "; 
-            }
-        }
     ?>
+
+    
 </body> 
 
 <?php include 'templates/footer.php'?> 
--- signup.inc.php
+++ signup.inc.php
@@ -1,9 +1,10 @@
 <?php 
     include_once 'connection.inc.php'; 
 
-    $userName = $_POST['user_name']; 
-    $userEmail = $_POST['user_email']; 
-    $userPassword = $_POST['user_pwd']; 
+    $userName = mysqli_real_escape_string($conn, $_POST['user_name']); 
+    $userEmail = mysqli_real_escape_string($conn, $_POST['user_email']); 
+    $userPassword = mysqli_real_escape_string($conn, $_POST['user_pwd']);
+     
     $sql = "INSERT INTO users (user_name, user_email, user_pwd) 
     VALUES ('$userName', '$userEmail', '$userPassword');"; 
 
