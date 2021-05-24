b121e0eee2755423369b1bd4223351f6e8672bdc
drain1ma <drain1ma@cmich.edu>
2021-05-24 02:10:11 -0400

    added placeholder logo

--- templates/header.php
+++ templates/header.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html> 
 <html> 
-<head>
+<head> 
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="css/site.css"> 
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <script src="lib/jquery-validation/dist/jquery.validate.js"></script>
@@ -13,10 +15,9 @@
 <header>
         <nav class="navbar navbar-expand-sm navbar-toggleable-sm navbar-light bg-white border-bottom box-shadow mb-3" style="text-align: right">
             <div class="container">
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".navbar-collapse" aria-controls="navbarSupportedContent"
-                        aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+                    <img src="img/gameinformerlogo.png" class="logo"/>
+                
+                <a style="background-image: url('img/gameinformer.png')"></a>
                 <div class="navbar-collapse collapse" id="collapsibleNavbar">
                     <ul class="navbar-nav">
                         <li class="nav-item">
