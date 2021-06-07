52706c4bbf72033bef759672c2dd0c6fa466f030
drain1ma <drain1ma@cmich.edu>
2021-06-06 22:09:42 -0400

    added profile.php

---
+++ profile.php
@@ -0,0 +1,12 @@
+
+<!DOCTYPE html> 
+<html>
+<?php
+    include 'templates/header.php'; 
+    include 'templates/footer.php'; 
+?>
+
+<body> 
+    <p>This is the profile page</p> 
+</body> 
+</html> 
